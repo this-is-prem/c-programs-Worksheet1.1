70eadb38a7f1b449b7437e9c2477f34fa679f06b
Premangshu <premangshu.banshi.29@gmail.com>
2025-11-04 11:36:07 +0530

    Revise main function for pattern printing
    Refactor main function to include row input and print a pattern based on the number of rows.

--- qn7.1.c
+++ qn7.1.c
@@ -1,9 +1,23 @@
 #include<stdio.h>
-int main(){
-int n ;
-printf("%d\n", n);
-printf("enter no of rows: ");
-scanf("%d", &n );
-printf("%d", n);
+int main () {
+int n;
+printf("enter no of rows :");
+scanf("%d", &n);
+for(int i =1 ; i<=n; i++){
+    if (i==1){
+        printf("#####\n");
+    }
+    else if (i>1 && i<n){
+        printf("#   #\n");
+    }
+    if(i==n){
+        printf("#####");
+    }
+
+}
+
+
+
 return 0;
 }
+
